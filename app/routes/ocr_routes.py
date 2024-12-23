63a95f76812f0b919135f93d5a76825d0b435653
Ken.C <39980945+KenCheungGameDesinger@users.noreply.github.com>
2024-12-23 22:28:53 +0900

    add: finetune solve model

--- app/routes/ocr_routes.py
+++ app/routes/ocr_routes.py
@@ -8,7 +8,6 @@ ocr_bp = Blueprint('ocr', __name__)
 @ocr_bp.route("/extract", methods=["POST"])
 def extract():
     try:
-        # latex_text = r"\[ \text{Simplify } \frac{(m^{5} n^{-2})^{6}}{m^{4} n^{-3}} \text{ and express your answer with positive indices.} \]"
         file = request.json.get("image_data", "")
         if (file == ""):
             return jsonify({"success": False, "error": "image_data: No image data provided."}), 400
--- app/services/solution_service.py
+++ app/services/solution_service.py
@@ -9,43 +9,83 @@ from typing import List
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
+# class SolutionModel(BaseModel):
+#     steps: List[str] = Field(..., description="List of solution steps in LaTeX format.")
+#     final_answer: str = Field(..., description="The final answer in LaTeX format.")
+
 def solve_math_problem(latex_equation):
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        # model="gpt-4o-mini",
+        model="ft:gpt-4o-2024-08-06:exmersive::AhbDUq1W",
         messages=[
+            # {
+            #     "role": "user",
+            #     "content": (
+            #         "Solve the given problem step by step in LaTeX format nested in JSON for expressing mathematics only. "
+            #         "Use the following structure: "
+            #         "- dont add and prefix or suffix to wrap the sentence in a LaTeX and json environment."
+            #         r"- every sentence should wrap by exact '\\text{}' but not '\\text{}' in Latex."
+            #         "- Do not add any explanatory text outside of LaTeX."
+            #         "- Wrap all equations in json format. "
+            #         "- the response json have to include 'topic', 'steps' and 'final answer'"
+            #         "- topics means the name of the problem in HKDSE mathematics. "
+            #         "- steps means the steps to solve the problem. Including instruction for each step. put each single step in to a list with Latex."
+            #         "- instructions in step is also in Latex format."
+            #         "- final answer means the final answer of the problem."
+            #         "- Include all steps explicitly in LaTeX with no text outside LaTeX formatting. "
+            #         "- don't add and prefix or suffix to wrap the json."
+            #         r"use '\newline' to break line for separating text sentences and math equation."
+            #         r"\\text{This step is incorrect because it does not relate to the original question.} \\text{You should start with the expression: } \\newline \\frac{(p^{4}q^{-3})^{5}}{p^{2}q^{-4}}."
+            #         "you must follow the habit in hand writing that start new line with each steps and each sentence with ':'"
+            #         "you should leave a space after text and before math equation"
+            #         # r"must put '\newline' before each '=' equal sign"
+            #         f"Problem: {latex_equation}"  # Dynamically embed the problem
+            #         "json format example: "
+            #         r'{"final answer": "m^{6} n^{6}","steps": ["\\text{Step 1: Start with the expression} \\newline \\left( \\frac{m^{5} n^{-2}}{m^{4} n^{-3}} \\right)^{6}","\\text{Step 2: Apply the power of a quotient rule} \\newline = \\frac{(m^{5} n^{-2})^{6}}{(m^{4} n^{-3})^{6}}","\\text{Step 3: Simplify the numerators and denominators using the power rule for exponents} \\newline = \\frac{m^{30} n^{-12}}{m^{24} n^{-18}}","\\text{Step 4: Apply the quotient rule for exponents on } m \\text{ and } n \\newline = m^{30-24} n^{-12 - (-18)}","\\text{Step 5: Simplify the exponents} \\newline = m^{6} n^{6}","\\text{Step 6: Write the final answer with positive indices} \\newline = m^{6} n^{6}"],"topic": "Simplifying Expressions with Indices" }'
+            #         "Latex Example: "
+            #         r'\\text{Simplify } \\left( \\frac{m^{5} n^{-2}}{m^{4} n^{-3}} \\right)^{6} \\text{ and express your answer with positive indices.}'
+            #     )
+            # }
+            {
+                "role": "system",
+                "content": """
+                The assistant is a math tutor that provides detailed, step-by-step solutions to math problems. 
+                All responses must be in JSON format and include:
+                1. `steps`: A list of solution steps, formatted in logical order.
+                2. `final_answer`: The final answer to the problem.
+                3. `topic`: Chapter in HKDSE
+                Latex content:
+                - use `\\newline` to before `=` and `:` 
+                - use space between text and mathematical expression: 'text\\'
+                All mathematical expressions must use LaTeX syntax compatible with React-MathQuill. 
+                Handle invalid or incomplete inputs gracefully by returning an error message in JSON format, explaining the issue.
+                Steps logic by Topics:
+                - Algebraic Exponents with Rational Expression
+                1. Apply exponent rules: Multiply powers in the fraction
+                2. Apply quotient rule: Subtract exponents with the same base
+                3. Convert negative exponents: Rewrite as positive exponents
+                4. Write final answer: Simplify and present in standard form
+                - 
+                """
+            },
             {
                 "role": "user",
                 "content": (
-                    "Solve the given problem step by step in LaTeX format nested in JSON for expressing mathematics only. "
-                    "Use the following structure: "
-                    "- dont add and prefix or suffix to wrap the sentence in a LaTeX and json environment."
-                    r"- every sentence should wrap by exact '\\text{}' but not '\\text{}' in Latex."
-                    "- Do not add any explanatory text outside of LaTeX."
-                    "- Wrap all equations in json format. "
-                    "- the response json have to include 'topic', 'steps' and 'final answer'"
-                    "- topics means the name of the problem in HKDSE mathematics. "
-                    "- steps means the steps to solve the problem. Including instruction for each step. put each single step in to a list with Latex."
-                    "- instructions in step is also in Latex format."
-                    "- final answer means the final answer of the problem."
-                    "- Include all steps explicitly in LaTeX with no text outside LaTeX formatting. "
-                    "- don't add and prefix or suffix to wrap the json."
-                    r"use '\newline' to break line for separating text sentences and math equation."
-                    r"\\text{This step is incorrect because it does not relate to the original question.} \\text{You should start with the expression: } \\newline \\frac{(p^{4}q^{-3})^{5}}{p^{2}q^{-4}}."
-                    "you must follow the habit in hand writing that start new line with each steps and each sentence with ':'"
-                    "you should leave a space after text and before math equation"
-                    # r"must put '\newline' before each '=' equal sign"
-                    f"Problem: {latex_equation}"  # Dynamically embed the problem
-                    "json format example: "
-                    r'{"final answer": "m^{6} n^{6}","steps": ["\\text{Step 1: Start with the expression} \\newline \\left( \\frac{m^{5} n^{-2}}{m^{4} n^{-3}} \\right)^{6}","\\text{Step 2: Apply the power of a quotient rule} \\newline = \\frac{(m^{5} n^{-2})^{6}}{(m^{4} n^{-3})^{6}}","\\text{Step 3: Simplify the numerators and denominators using the power rule for exponents} \\newline = \\frac{m^{30} n^{-12}}{m^{24} n^{-18}}","\\text{Step 4: Apply the quotient rule for exponents on } m \\text{ and } n \\newline = m^{30-24} n^{-12 - (-18)}","\\text{Step 5: Simplify the exponents} \\newline = m^{6} n^{6}","\\text{Step 6: Write the final answer with positive indices} \\newline = m^{6} n^{6}"],"topic": "Simplifying Expressions with Indices" }'
-                    "Latex Example: "
-                    r'\\text{Simplify } \\left( \\frac{m^{5} n^{-2}}{m^{4} n^{-3}} \\right)^{6} \\text{ and express your answer with positive indices.}'
-                )
+                    "response json format example: "
+                    "{{steps:str[], final_answer:str}, topic: str}"
+                    r'{{"final answer": "m^{6} n^{6}","steps": ["\\text{Step 1: Start with the expression} \\newline \\left( \\frac{m^{5} n^{-2}}{m^{4} n^{-3}} \\right)^{6}","\\text{Step 2: Apply the power of a quotient rule} \\newline = \\frac{(m^{5} n^{-2})^{6}}{(m^{4} n^{-3})^{6}}","\\text{Step 3: Simplify the numerators and denominators using the power rule for exponents} \\newline = \\frac{m^{30} n^{-12}}{m^{24} n^{-18}}","\\text{Step 4: Apply the quotient rule for exponents on } m \\text{ and } n \\newline = m^{30-24} n^{-12 - (-18)}","\\text{Step 5: Simplify the exponents} \\newline = m^{6} n^{6}","\\text{Step 6: Write the final answer with positive indices} \\newline = m^{6} n^{6}"],"topic": "Simplifying Expressions with Indices" }}'
+                )},
+            {
+                "role": "user",
+                "content": f"Problem: {latex_equation}"
             }
         ]
     )
 
     response = response.choices[0].message.content
-    # print(response)
+    response = response.replace("```json", "").replace("```", "")
+
+    print(response)
     return json.loads(response)
 
 
